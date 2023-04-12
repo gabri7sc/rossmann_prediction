c3e5ae2e520d4903e8e5dce4bd1ebd5051bb74a6
Gabriel <102957524+gabri7sc@users.noreply.github.com>
2023-04-12 05:41:16 -0300

    Update handler.py

--- handler.py
+++ handler.py
@@ -1,18 +1,17 @@
-import os
 import pickle
 import pandas as pd
 from flask import Flask, request, Response
 from rossmann.Rossmann import Rossmann
+import os
 
-# loading model local Test
-
-model = pickle.load(open( 'https://github.com/gabri7sc/rossmann_prediction/tree/main/model/model_rossmann.pkl', 'rb' ) )
+# loading model
+model = pickle.load(open(
+    'model/model_rossmann.pkl', 'rb'))
 
 # initialize API
 app = Flask(__name__)
 
-
-@app.route('/rossmann/predict', methods=['POST'])
+@app.route('/rossmann/predict', methods=['GET', 'POST'])
 def rossmann_predict():
     test_json = request.get_json()
 
@@ -40,9 +39,10 @@ def rossmann_predict():
 
         return df_response
 
+
     else:
         return Reponse('{}', status=200, mimetype='application/json')
 
 if __name__ == '__main__':
     port = os.environ.get( 'PORT', 5000 )
-    app.run( host='0.0.0.0', port = port)
+    app.run( host='0.0.0.0', port=port )
